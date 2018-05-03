1617200d88c612a85cd105f224d277acfd038968
Florent ALAPETITE <E166319G@S046V7pc01.ensinfo.sciences.univ-nantes.prive>
2018-05-03 14:14:30 +0200

    Changes to the model to handle the data modification

--- source/model.py
+++ source/model.py
@@ -2,7 +2,6 @@ import pandas
 from sklearn import linear_model
 import csv
 
-
 df = pandas.read_csv("../data/cleaned_learning.csv", sep=";")
 
 classe_emprunt = df.iloc[:, 5]
@@ -10,15 +9,8 @@ classe_previsionnel = df.iloc[:, 6]
 
 df = df.iloc[:, [x for x in range(df.shape[1]) if x not in [0, 1, 2, 3, 5, 6]]]
 
-
-classifiers = [
-    linear_model.BayesianRidge,
-    linear_model.LassoLars,
-    linear_model.LinearRegression]
-
-
 test = pandas.read_csv("../data/cleaned_test.csv", sep=";")
-test = test.iloc[:, :-5]
+test = test.iloc[:, 1:-5]
 
 
 def prediction_emprunt(classifier, filename):
@@ -36,21 +28,30 @@ def prediction_previsionnel(classifier, filename):
 
     return prediction
 
+def emprunt_previsionnel_prediction():
+    classifiers = [
+        linear_model.BayesianRidge,
+        linear_model.LassoLars,
+        linear_model.LinearRegression]
 
-for item in classifiers:
-    filename = "../data/predicted_" + item.__name__ + ".csv"
-    pred_emprunt = prediction_emprunt(item, filename)
-    pred_previsionnel = prediction_previsionnel(item, filename)
+    for item in classifiers:
+        filename = "../data/predicted_" + item.__name__ + ".csv"
+        pred_emprunt = prediction_emprunt(item, filename)
+        pred_previsionnel = prediction_previsionnel(item, filename)
 
-    output_file = open(filename, 'wt')
-    output_writer = csv.writer(output_file)
+        output_file = open(filename, 'wt')
+        output_writer = csv.writer(output_file)
 
-    with open("../data/cleaned_test.csv", 'rt') as csvfile:
-        result_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
-        i = 0
-        for line in result_reader:
-            if i != 0:
-                line[31] = pred_emprunt[i - 1]
-                line[32] = pred_previsionnel[i - 1]
-            i = i + 1
-            output_writer.writerow(line)
+        with open("../data/cleaned_test.csv", 'rt') as csvfile:
+            result_reader = csv.reader(csvfile, delimiter=';', quotechar='"')
+            i = 0
+            for line in result_reader:
+                if i != 0:
+                    line[31] = pred_emprunt[i - 1]
+                    line[32] = pred_previsionnel[i - 1]
+                i = i + 1
+                output_writer.writerow(line)
+
+
+
+
