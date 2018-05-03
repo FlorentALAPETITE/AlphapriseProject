21b77de341218e0b02cb6a0fbd7257acc4392666
Florent ALAPETITE <E166319G@S046V7pc01.ensinfo.sciences.univ-nantes.prive>
2018-05-03 15:20:35 +0200

    Fix model implementation

--- source/model.py
+++ source/model.py
@@ -11,32 +11,34 @@ CLASSIFIERS = [
 ]
 
 
-def prediction_emprunt(classifier, df_app, df_test, class_index=5):
-    classe_emprunt = df.iloc[:, class_index]
-    classifier.fit(df_app, classe_emprunt)
-    prediction = classifier.predict(df_test)
+def prediction_emprunt(classifier, training_dataframe, test_dataframe, full_training_dataframe, class_index=5):
+    classe_emprunt = full_training_dataframe.iloc[:, class_index]
+    classifier = classifier()
+    classifier.fit(training_dataframe, classe_emprunt)
+    prediction = classifier.predict(test_dataframe)
 
     return prediction
 
 
-def prediction_previsionnel(classifier, df_app, df_test, class_index=6):
-    classe_previsionnel = df.iloc[:, class_index]
-    classifier.fit(df_app, classe_previsionnel)
-    prediction = classifier.predict(df_test)
+def prediction_previsionnel(classifier, training_dataframe, test_dataframe, full_training_dataframe, class_index=6):
+    classe_previsionnel = full_training_dataframe.iloc[:, class_index]  
+    classifier = classifier()  
+    classifier.fit(training_dataframe, classe_previsionnel)
+    prediction = classifier.predict(test_dataframe)
 
     return prediction
 
 def emprunt_previsionnel_prediction():
-    df = pandas.read_csv("../data/cleaned_learning.csv", sep=";")
-    df = df.iloc[:, [x for x in range(df.shape[1]) if x not in [0, 1, 2, 3, 5, 6]]]
+    full_training_dataframe = pandas.read_csv("../data/cleaned_learning.csv", sep=";")
+    training_dataframe = full_training_dataframe.iloc[:, [x for x in range(full_training_dataframe.shape[1]) if x not in [0, 1, 2, 3, 5, 6]]]
 
     test = pandas.read_csv("../data/cleaned_test.csv", sep=";")
     test = test.iloc[:, 1:-5]
 
     for item in CLASSIFIERS:
         filename = "../data/predicted_" + item.__name__ + ".csv"
-        pred_emprunt = prediction_emprunt(item)
-        pred_previsionnel = prediction_previsionnel(item)
+        pred_emprunt = prediction_emprunt(item,training_dataframe,test,full_training_dataframe)
+        pred_previsionnel = prediction_previsionnel(item,training_dataframe,test,full_training_dataframe)
 
         output_file = open(filename, 'wt')
         output_writer = csv.writer(output_file)
@@ -50,3 +52,6 @@ def emprunt_previsionnel_prediction():
                     line[COL_PREVISIO] = pred_previsionnel[i - 1]
                 i = i + 1
                 output_writer.writerow(line)
+
+
+emprunt_previsionnel_prediction()
